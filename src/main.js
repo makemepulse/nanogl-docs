4f2c54aaabc2878928c1a9076ddfa0351e867d97
Romane Chouteau <romane.chouteau@makemepulse.com>
2023-11-03 11:28:42 +0100

    :sparkles: redirect getting started to first article in section

--- src/main.js
+++ src/main.js
@@ -6,7 +6,13 @@ import App from './App.vue'
 import routes from '~pages'
 
 const router = createRouter({
-  routes,
+  routes: [
+    ...routes,
+    {
+      path: '/guide/getting-started',
+      redirect: '/guide/getting-started/installation',
+    },
+  ],
   history: createWebHistory(),
   scrollBehavior(to, from, savedPosition) {
     if (to.hash) {
