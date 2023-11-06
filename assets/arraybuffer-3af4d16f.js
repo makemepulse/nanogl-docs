20f1fbba3073bfe56ea15af69dfef36e3e077a41
romanechouteau <59968685+romanechouteau@users.noreply.github.com>
2023-11-06 16:52:45 +0000

    Deploying to gh-pages from @ makemepulse/nanogl-docs@f9b0c220db0a9190c3889c75dc411430e9130d70 🚀

--- assets/arraybuffer-3af4d16f.js
+++ assets/arraybuffer-3af4d16f.js
@@ -1,4 +1,4 @@
-import{b as a}from"./route-block-83d24a4e.js";import{o as r,c as t,b as o}from"./index-0b2e41f6.js";const n={class:"markdown-body"},f=o(`<h1>ArrayBuffer</h1><p>Manage ARRAY_BUFFER type Arraybuffers.</p><p>##exemples</p><h5>ArrayBuffer creation</h5><pre class="language-JavaScript"><code class="language-JavaScript">// create empty Arraybuffer
+import{b as a}from"./route-block-83d24a4e.js";import{o as r,c as t,b as o}from"./index-d5dc1d19.js";const n={class:"markdown-body"},f=o(`<h1>ArrayBuffer</h1><p>Manage ARRAY_BUFFER type Arraybuffers.</p><p>##exemples</p><h5>ArrayBuffer creation</h5><pre class="language-JavaScript"><code class="language-JavaScript">// create empty Arraybuffer
 var buffer = new ArrayBuffer( gl );
 
 // you can also provide initial data and optional usage hint
