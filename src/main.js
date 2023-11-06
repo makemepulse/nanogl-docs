29df6e7c1d4cffd36c671cba0a3f6cc6e23a32f2
Romane Chouteau <romane.chouteau@makemepulse.com>
2023-11-06 17:50:47 +0100

    :bug: try to fix catch all route

--- src/main.js
+++ src/main.js
@@ -5,25 +5,36 @@ import './styles/globals.css'
 import App from './App.vue'
 import routes from '~pages'
 
+import NotFound from './components/NotFound.vue'
+
 const router = createRouter({
   routes: [
     ...routes,
     {
       path: '/guide',
+      name: 'guide',
       redirect: '/guide/getting-started/installation',
     },
     {
       path: '/docs',
+      name: 'docs',
       redirect: '/docs/nanogl-node',
     },
     {
       path: '/guide/getting-started',
+      name: 'getting-started',
       redirect: '/guide/getting-started/installation',
     },
     {
       path: '/guide/quick-guides',
+      name: 'quick-guides',
       redirect: '/guide/quick-guides/texture-2d',
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFound
+    }
   ],
   history: createWebHistory(import.meta.env.VITE_APP_BASE_URL || '/'),
   scrollBehavior(to, from, savedPosition) {
--- src/pages/[...all].vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <NotFound />
-</template>
