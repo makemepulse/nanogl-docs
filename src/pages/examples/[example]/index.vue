82054760f4729fbed6a4b8c0e67d274cb84875b2
Romane Chouteau <romane.chouteau@makemepulse.com>
2023-11-10 19:27:02 +0100

    :lipstick: adjust examples page layout

--- src/pages/examples/[example]/index.vue
+++ src/pages/examples/[example]/index.vue
@@ -1,7 +1,7 @@
 <template>
   <NotFound v-if="!currentExample" />
   <div v-else class="w-full h-full page-examples">
-    <div class="relative z-[1] w-full h-full flex flex-col justify-between pointer-events-none overflow-hidden">
+    <div class="relative z-[1] w-full h-full flex justify-between pointer-events-none overflow-hidden">
       <div class="self-start w-fit lg:max-w-[33%] p-12 flex flex-col bg-light-grey rounded-md pointer-events-auto">
         <h1 class="text-16">
           {{ currentExample.category }} — {{ currentExample.name }}
@@ -11,20 +11,22 @@
           class="text-14 mt-4"
           v-html="exampleDescription" />
       </div>
-      <a
-        :href="SOURCE_PATH + currentExample.id + '.js'"
-        target="_blank"
-        class="custom-link self-end bg-light-grey rounded-md overflow-hidden pointer-events-auto"
-      >
-        <div class="hover:bg-white-5 flex items-center justify-center gap-10 px-12 py-8 transition-colors">
-          <p class="text-16 font-bold">View code</p>
-          <UIIcon
-            name="github"
-            class="h-20 w-auto"
-          />
-        </div>
-      </a>
-      <div id="debug" class="absolute top-24 right-24 pointer-events-auto"></div>
+      <div class="h-full flex flex-col-reverse justify-between">
+        <a
+          :href="SOURCE_PATH + currentExample.id + '.js'"
+          target="_blank"
+          class="custom-link self-end bg-light-grey rounded-md overflow-hidden pointer-events-auto"
+        >
+          <div class="hover:bg-white-5 flex items-center justify-center gap-10 px-12 py-8 transition-colors">
+            <p class="text-16 font-bold">View code</p>
+            <UIIcon
+              name="github"
+              class="h-20 w-auto"
+            />
+          </div>
+        </a>
+        <div id="debug" class="pointer-events-auto"></div>
+      </div>
     </div>
     <GLPreview :name="exampleName" folder="examples" class="absolute top-0 left-0 w-full z-0" />
   </div>
