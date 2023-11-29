aaded22f0a4f2c00733037b71c1b66a5b20f0d82
Romane Chouteau <romane.chouteau@makemepulse.com>
2023-11-29 12:09:09 +0100

    :construction: prepare starter preview iframe

---
+++ src/components/StarterPreview.vue
@@ -0,0 +1,11 @@
+<template>
+  <iframe
+    :src="SOURCE"
+    class="w-full max-w-[800px] mx-auto bg-grey-50 aspect-[4/3] my-32 rounded-md"
+  >
+  </iframe>
+</template>
+
+<script setup lang="ts">
+const SOURCE = 'https://makemepulse.github.io/nanogl-docs/examples/triangle';
+</script>
